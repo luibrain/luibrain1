ba0a35975c2b85e37f37206779ee34662f4f697f
luibrain <luibrain2301@gmail.com>
2019-03-19 15:27:14 +0300

    Add files via upload

---
+++ server.r
@@ -0,0 +1,41 @@
+library('shiny')       # загрузка пакетов
+library('lattice')
+
+file.URL <- 'https://raw.githubusercontent.com/luibrain/luibrain1/laba3/Films-imdb2016.csv'
+download.file(file.URL, destfile = 'Films.csv')
+
+df <- data.table(read.csv('Films.csv', 
+                          stringsAsFactors = F))
+
+shinyServer(function(input, output) {
+    # текст для отображения на главной панели
+    output$gn.text <- renderText({
+        paste0('Вы выбрали жанр: ', 
+               input$gn.to.plot # переменная, связанная со списком возраста
+               )})
+    output$gn.text1 <- renderText({
+      paste0('Вы выбрали оценку с ', 
+             input$Rating.range[1], ' по ', input$Rating.range[2]
+             )
+    })
+    output$gn.text2 <- renderText({
+      paste0('Вы выбрали продолжительность с ',
+             input$Runtime.range[1], ' по ', input$Runtime.range[2]
+             )
+    })
+    # строим гистограммы переменных
+    output$gn.hist <- renderPlot({
+        # сначала фильтруем данные
+        DF <- df[df$Genre == input$gn.to.plot, 1:8]
+        DF <- DF[between(DF$Rating, input$Rating.range[1], input$Rating.range[2])]
+        DF <- DF[between(DF$Runtime, input$Runtime.range[1], input$Runtime.range[2])]
+
+        # затем строим график
+        histogram( ~ Gross_Earning_in_Mil, 
+                   data = DF,
+                   xlab = '',
+                  breaks = seq(min(DF$Gross_Earning_in_Mil), max(DF$Gross_Earning_in_Mil), 
+                               length = input$int.hist + 1)
+                   )
+    })
+})
---
+++ ui.r
@@ -0,0 +1,51 @@
+library('shiny')       # загрузка пакетов
+library('data.table')
+
+file.URL <- 'https://raw.githubusercontent.com/luibrain/luibrain1/laba3/Films-imdb2016.csv'
+download.file(file.URL, destfile = 'Films.csv')
+
+df <- data.table(read.csv('Films.csv', 
+                          stringsAsFactors = F))
+
+# список уникальных значений столбца Жанр
+gn.filter <- as.character(unique(df$Genre))
+names(gn.filter) <- gn.filter
+gn.filter <- as.list(gn.filter)
+
+# размещение всех объектов на странице
+shinyUI(
+    # создать страницу с боковой панелью
+    # и главной областью для отчётов
+    pageWithSidebar(
+        # название приложения:
+        headerPanel('Кассовые сборы фильмов за 2016 год на портале IMDB'),
+        # боковая панель:
+        sidebarPanel(
+            selectInput(               # выпадающее меню: возраст
+                'gn.to.plot',          # связанная переменная
+                        'Выберите жанр',  # подпись списка
+                        gn.filter),            # сам список
+            
+            sliderInput('Rating.range', 'Оценка:',
+                        min = min(df$Rating), max = max(df$Rating), value = c(min(df$Rating), max(df$Rating))),
+            
+            sliderInput('Runtime.range', 'Продолжительность:',
+                        min = min(df$Runtime), max = max(df$Runtime), value = c(min(df$Runtime), max(df$Runtime))),
+            sliderInput(               # слайдер: кол-во интервалов гистограммы
+              'int.hist',                       # связанная переменная
+              'Количество интервалов гистограммы:', # подпись
+              min = 2, max = 10,                    # мин и макс
+              value = floor(1 + log(50, base = 2)), # базовое значение
+              step = 1)                             # шаг
+        ),
+        # главная область
+        mainPanel(
+            # текстовый объект для отображения
+            textOutput('gn.text'),
+            textOutput('gn.text1'),
+            textOutput('gn.text2'),
+            # гистограммы переменных
+            plotOutput('gn.hist')
+            )
+        )
+    )
